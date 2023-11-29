4b5f5d49410d8c4284c4862dac4db0076674852e
Filip M <filipm93@gmail.com>
2023-11-29 22:11:58 +0100

    home-screen

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,5 +1,36 @@
-import { Text } from 'react-native-paper';
+import { SvgXml } from 'react-native-svg';
+import logo from '../../assets/logo';
+import styled from 'styled-components/native';
+import { Surface, Text } from 'react-native-paper';
+import { View } from 'react-native';
+
+const LogoContainer = styled(Surface)`
+  background-color: ${({ theme }) => theme.colors.primaryDark};
+  border-radius: 0 0 10px 10px;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container = styled(View)`
+  height: 400px;
+`;
+
+const Sub = styled(Text)`
+  color: ${({ theme }) => theme.colors.primaryLight};
+  font-size: 20px;
+  margin-top: 50px;
+`;
 
 export function HomeScreen() {
-  return <Text>Home Screen</Text>;
+  return (
+    <>
+      <Container>
+        <LogoContainer elevation={2}>
+          <SvgXml xml={logo} width={210} height={210} />
+          <Sub>Tap to take a photo!</Sub>
+        </LogoContainer>
+      </Container>
+    </>
+  );
 }
